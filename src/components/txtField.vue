8415930fa17e9c3297907ed0b3259bb17529ce65
saman-re <saman.rezapoor1379@yahoo.com>
2021-06-14 00:47:24 +0430

    txtField styling done

--- src/components/txtField.vue
+++ src/components/txtField.vue
@@ -1,30 +1,36 @@
 <template>
   <div class="right-inner-addon input-container">
-    <i class="fa fa-search"></i>
-    <input type="text" class="form-control" placeholder="Right icon" />
+    <i class="material-icons search-icon">search</i>
+    <input class="search-input" type="text" placeholder="جستجو کنید" />
   </div>
 </template>
 <style lang="scss" scoped>
+@import '../assets/_variables.scss';
+
 .input-container {
+  position: relative;
   padding-bottom: 1em;
 }
+.search-input {
+  width: 100%;
+  padding: 1.2em;
+  padding-right: 35px;
+  margin: 0em;
+  box-sizing: border-box;
+  direction: rtl;
+  border-radius: 10px;
+  background-color: $base-color;
+  border:$box-border-color;
 
-.right-inner-addon {
-  position: relative;
+  &:focus{
+    border:$box-border-color;
+  }
 }
-.right-inner-addon input {
-  padding-right: 35px !important;
-}
-.right-inner-addon i {
+.search-icon {
   position: absolute;
   right: 0px;
   padding: 12px 12px;
   pointer-events: none;
-}
-input {
-  width: 100%;
-  padding: 1em !important;
-  margin: 0em !important;
-  box-sizing: border-box;
+  color: darken($box-border-color,40%);
 }
 </style>
