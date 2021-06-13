357354d5ff6223200a1bb9ee729d6a9727fc5897
saman-re <saman.rezapoor1379@yahoo.com>
2021-06-13 18:49:39 +0430

    toggler style refactor

--- src/components/toggler.vue
+++ src/components/toggler.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="toggler">
     <input type="checkbox" id="switch" @change="toggle" v-model="toggleMode" />
     <label for="switch">Toggle</label>
   </div>
@@ -19,56 +19,56 @@ export default {
 };
 </script>
 <style lang="scss">
-@import "../assets/_variables.scss";
+@import '../assets/_variables.scss';
 
-$to-width:40px;
-$to-height:21px;
-$pin-size:15px;
-$spacing:4px;
-$border:1px solid #8c8c8c;
+$toggle-width: 40px;
+$toggle-height: 21px;
+$pin-size: 15px;
+$spacing: 4px;
 
-input[type='checkbox'] {
+$border: 1px solid #8c8c8c;
+
+input {
   height: 0;
   width: 0;
   visibility: hidden;
+
+  &:checked + label {
+    background: $primary;
+  }
+
+  &:checked + label::after {
+    right: calc(100% - 2.5px);
+    -webkit-transform: translateX(100%);
+    -ms-transform: translateX(100%);
+    transform: translateX(100%);
+  }
 }
 
 label {
   cursor: pointer;
   text-indent: -9999px;
-  width: $to-width;
-  height: $to-height;
+  width: $toggle-width;
+  height: $toggle-height;
   background: $base-color;
   display: block;
   border: $border;
-  border-radius: $to-height;
+  border-radius: $toggle-height;
   position: relative;
-}
 
-label:after {
-  content: '';
-  position: absolute;
-  top: $spacing/2;
-  right: $spacing/2;
-  width: $pin-size;
-  height: $pin-size;
-  background: #fff;
-  border: $border;
-  border-radius: $pin-size;
-  transition: 0.3s;
-}
-
-input:checked + label {
-  background: $primary;
-  
-}
-
-input:checked + label:after {
-  right: calc(100% - 2.5px);
-  transform: translateX(100%);
-}
-
-label:active:after {
-  width: $pin-size;
+  &::after {
+    content: '';
+    position: absolute;
+    top: $spacing/2;
+    right: $spacing/2;
+    width: $pin-size;
+    height: $pin-size;
+    background: #fff;
+    border: $border;
+    border-radius: $pin-size;
+    -webkit-transition: 0.3s;
+    -o-transition: 0.3s;
+    transition: 0.3s;
+  }
 }
 </style>
